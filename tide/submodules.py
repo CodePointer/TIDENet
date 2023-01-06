d10ddecb1c8d51a81e46bc2d8164f07fff1b93d4
Rukun Qiao <CodePointer@users.noreply.github.com>
2023-01-06 14:37:50 +0800

    Increase the TIDEFeature encoder layers.

--- tide/submodules.py
+++ tide/submodules.py
@@ -223,27 +223,39 @@ class SmallEncoder(nn.Module):
         super(SmallEncoder, self).__init__()
         self.norm_fn = norm_fn
 
+        # output_dim = 128
+        # conv1_out = 32
+        # layer1_out = 32
+        # layer2_out = 64
+        # layer3_out = 96
+
+        output_dim = 256
+        conv1_out = 64
+        layer1_out = 64
+        layer2_out = 128
+        layer3_out = 256
+
         if self.norm_fn == 'group':
             self.norm1 = nn.GroupNorm(num_groups=8, num_channels=32)
 
         elif self.norm_fn == 'batch':
-            self.norm1 = nn.BatchNorm2d(32)
+            self.norm1 = nn.BatchNorm2d(conv1_out)
 
         elif self.norm_fn == 'instance':
-            self.norm1 = nn.InstanceNorm2d(32)
+            self.norm1 = nn.InstanceNorm2d(conv1_out)
 
         elif self.norm_fn == 'none':
             self.norm1 = nn.Sequential()
 
-        self.conv1 = nn.Conv2d(input_dim, 32, kernel_size=7, stride=2, padding=3)
+        self.conv1 = nn.Conv2d(input_dim, conv1_out, kernel_size=7, stride=2, padding=3)
         self.relu1 = nn.ReLU(inplace=True)
 
-        self.in_planes = 32
-        self.layer1 = self._make_layer(32, stride=1)
-        self.layer2 = self._make_layer(64, stride=2)
-        self.layer3 = self._make_layer(96, stride=2)
+        self.in_planes = conv1_out
+        self.layer1 = self._make_layer(layer1_out, stride=1)
+        self.layer2 = self._make_layer(layer2_out, stride=2)
+        self.layer3 = self._make_layer(layer3_out, stride=2)
 
-        self.conv2 = nn.Conv2d(96, output_dim, kernel_size=1)
+        self.conv2 = nn.Conv2d(layer3_out, output_dim, kernel_size=1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -264,14 +276,14 @@ class SmallEncoder(nn.Module):
 
     def forward(self, x):
         # if input is list, combine batch dimension
-        x = self.conv1(x)
+        x = self.conv1(x)   # input_dim -> 32
         x = self.norm1(x)
         x = self.relu1(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.conv2(x)
+        x = self.layer1(x)  # 32 -> 32
+        x = self.layer2(x)  # 32 -> 64
+        x = self.layer3(x)  # 64 -> 96
+        x = self.conv2(x)   # 96 -> 128
 
         return x
 
